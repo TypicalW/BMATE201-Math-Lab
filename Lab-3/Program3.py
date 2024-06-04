d13f67ce06b81e725c66c9792cf6174a57749798
TypicalW <ashmit1665@gmail.com>
2024-06-05 01:33:40 +0530

    Added program3

---
+++ Lab-3/Program3.py
@@ -0,0 +1,23 @@
+import sys 
+from sympy import *
+var("x")
+f=1/(1+x**2)
+x0=float(input("Enter the lower limit "))
+xn=float(input("Enter the upper limit "))
+n=int(input("Enter number of sub intervals "))
+if n%3!=0:
+    sys.exit("The value of x should be divisble by 2")
+print("Value of integration  by regular method is %.3f"%integrate(f,(x,x0,xn)))
+f=lambdify(x,f)
+h=(xn-x0)/n
+sum=0
+for i in range(0,n+1):
+    xi=x0+i*h
+    print(f"x{i}=%.2f\t\tf(%.2f)=%.2f"%(xi,xi,f(xi)))
+    if i==0 or i==n:
+        sum=sum+f(xi)
+    elif i%3==0:
+        sum=sum+2*f(xi)
+    else:
+        sum=sum+3*f(xi)
+print("Value of the integral by Simpson's (3/8)rd Rule is %0.3f" % (sum*3*h/8))
